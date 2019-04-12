3d432fc2f3d79cf9b61d89718f63952958d0fd15
gaborszekely <gabor_szekely@hotmail.com>
2019-04-12 16:10:54 -0600

    Fixed CORS issue

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const router = require('./router');
 const port = process.env.PORT || 3005;
 
@@ -11,6 +12,7 @@ mongoose
   .then(() => console.log('Mongoose connected'))
   .catch(console.error);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
